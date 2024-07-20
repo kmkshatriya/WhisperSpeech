00c800c1b0617eebe3b62653365894a2315e9af6
kmkshatriya <139339313+kmkshatriya@users.noreply.github.com>
2024-07-20 15:01:47 +0530

    offline gpu version

--- app.py
+++ app.py
@@ -1,40 +1,64 @@
+# app.py --txt "Hello, how are you doing today"
+
+
+import os
 import argparse
 from whisperspeech.pipeline import Pipeline
 
 #---------------------------------------------
+code_dir = os.getcwd()
 parser = argparse.ArgumentParser(description='Code to convert ext to speech using WhisperSpeech')
 
 parser.add_argument(
   '--txt', 
   type=str,
   help='Input text to be converted to speech.', 
-  default='Hello! how are you doing today
-)
-
-parser.add_argument(
-  '--speaker', 
-  type=str,
-  help='Path of reference speaker audio to be cloned.', 
-  default='input/a.wav'
-)
+  default='Testing text to speech using whisperspeech'
+  )
 
 parser.add_argument(
   '--outfile',
   type=str,
   help='Path to save the output.', 
   default='results/r.wav'
-)
+  )
+
+parser.add_argument(
+  '--clone', 
+  action='store_true', 
+  help='If True, then the default/given speaker voice would be used',
+  )
+
+parser.add_argument(
+  '--speaker', 
+  type=str,
+  help='Path of reference speaker audio to be cloned.', 
+  default='input/a.wav'
+  )
 
 args = parser.parse_args()
 #---------------------------------------------
+
 def main():
+
   pipe = Pipeline(
-      s2a_ref=f'{p_ws_model}models/s2a-q4-tiny-en+pl.model', 
-      t2s_ref=f'{p_ws_model}/t2s-small-en+pl.model', 
-      voc_ref=p_ve_model,    
-      spbr_ref=p_sb_model,
+      s2a_ref=f'{code_dir}/models/whisp/s2a-q4-tiny-en+pl.model', 
+      t2s_ref=f'{code_dir}/models/whisp/t2s-small-en+pl.model', 
+      voc_ref=f'{code_dir}/models/vocos',    
+      spbr_ref=f'{code_dir}/models/speechbrain',
       device=device
   )
-  
+  if args.clone:
+      pipe.generate_to_file(
+        args.outfile, 
+        args.txt, 
+        lang='en', 
+        cps=10.5, 
+        speaker=args.speaker
+        )  
+  else:
+      pipe.generate_to_file(args.outfile, args.txt)
+      
+#---------------------------------------------
 if __name__ == '__main__':
 	main()
