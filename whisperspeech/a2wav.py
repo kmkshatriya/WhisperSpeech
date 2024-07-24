42e3dcbfc96b485f65e18a1527d6eafa29296e99
kmkshatriya <139339313+kmkshatriya@users.noreply.github.com>
2024-07-24 21:01:58 +0530

    k

--- whisperspeech/a2wav.py
+++ whisperspeech/a2wav.py
@@ -11,7 +11,7 @@ import torchaudio
 
 # %% ../nbs/6. Quality-boosting vocoder.ipynb 2
 class Vocoder:
-    def __init__(self, repo_id="charactr/vocos-encodec-24khz", local_pth=None, device=None, enc_repo=None):
+    def __init__(self, repo_id="charactr/vocos-encodec-24khz", local_pth=None, device=None, enc_ref=None):
         if device is None: device = inference.get_compute_device()
         if device == 'mps': device = 'cpu' # mps does not currently work with vocos, thus only cuda or cpu
         self.device = device
